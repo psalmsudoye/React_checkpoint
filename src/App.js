36adc649c3d91526a573b29ee3c339fd17e4966e
PSALMSUDOYE <SAMUELUDOYE77@GMAIL.COM>
2023-04-11 23:46:29 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+// import './app.css'
+// import {Routes,Route} from 'react-router'  
+// import Home from './pages/home/Home'
+// import Contact from './pages/Contact/Contact'
+// import About from './pages/about/About'
+// import Header from './components/header/Header'
+import Greetings from './pages/greetings/Greetings'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+    <div>
+      {/* <Header />
+      <div className='App'>
+      {/* <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+      </Routes> */} 
+      {/* <Greetings name="maria" />
+      <Greetings name="etuk" />
+      <Greetings name="afolabi" />
+      <Greetings name="taiwo" />
+     */}
+     <Greetings/>
+      </div>
+    
+
   );
 }
 
